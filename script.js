18f9548fa8d78b9312d19216df1974e391ee1fc1
Leandro-Frota <leandromfrota@gmail.com>
2024-01-19 17:36:50 -0300

    Estilizar botões e inputs e inserir e mostrar o resultado do calculo da gorjeta

--- script.js
+++ script.js
@@ -1,36 +1,51 @@
+const form = document.querySelector('form');
 const inputBill = document.getElementById('inputBill');
 const btn5Perc = document.getElementById('btn5Perc');
 const btn10Perc = document.getElementById('btn10Perc');
 const btn15Perc = document.getElementById('btn15Perc');
 const btn25Perc = document.getElementById('btn25Perc');
 const btn50Perc = document.getElementById('btn50Perc');
-const form = document.querySelector('form');
+const tipAmountResult = document.getElementById('tipAmountResult')
+const inputPeople = document.getElementById('inputPeople')
 
-console.log(form)
 
 form.addEventListener('click',function(e){
     const evento = e.target
+    const people = inputPeople.value
+    if(!people){
+        alert("insira o numero de pessoas")
+    }
     if(evento == btn5Perc){
-    const result = inputBill.value*0.05
-    console.log(result)
-    console.log(evento)
+ 
+    result = (inputBill.value*0.05)/people
+    tipAmountResult.innerText = result
+     
+  
+
     }else if(evento==btn10Perc){
         const result = inputBill.value*0.10
-        console.log(result)
-        console.log(evento)
+        tipAmountResult.innerText = result
 
     }else if(evento==btn15Perc){
+
         const result = inputBill.value*0.15
-        console.log(result)
-        console.log(evento)
+        tipAmountResult.innerText = result
+
     }else if(evento==btn25Perc){
+
         const result = inputBill.value*0.25
-        console.log(result)
-        console.log(evento)
+        tipAmountResult.innerText = result
+
     }else if(btn50Perc){
+        
         const result = inputBill.value*0.50
-        console.log(result)
-        console.log(evento)
+        tipAmountResult.innerText = result
+
+    }else{
+        evento=" "
+        result = 0
     }
+
+    
 })
 
