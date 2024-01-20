9cd6e24c859e84796bf72ac1defd2cc206664d7c
Leandro-Frota <108706993+Leandro-Frota@users.noreply.github.com>
2024-01-20 17:02:16 -0300

    Adicionei validação para o campo input People e ajustei bug ao clicar nos botões percentuais

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const form = document.querySelector('form');
+const buttonSelectTips = document.getElementById('buttonSelectTips')
 const inputBill = document.getElementById('inputBill');
 const btn5Perc = document.getElementById('btn5Perc');
 const btn10Perc = document.getElementById('btn10Perc');
@@ -7,18 +8,28 @@ const btn25Perc = document.getElementById('btn25Perc');
 const btn50Perc = document.getElementById('btn50Perc');
 const tipAmountResult = document.getElementById('tipAmountResult')
 const inputPeople = document.getElementById('inputPeople')
+const inputPeopleContent= document.getElementById('inputPeopleContent')
 
 
-form.addEventListener('click',function(e){
+buttonSelectTips.addEventListener('click',function(e){
     const evento = e.target
     const people = inputPeople.value
-    if(!people){
-        alert("insira o numero de pessoas")
-    }
+    errorPeople.innerText = ""
+ 
+   
     if(evento == btn5Perc){
  
     result = (inputBill.value*0.05)/people
-    tipAmountResult.innerText = result
+
+    if(result!=NaN && result!=Infinity){
+        tipAmountResult.innerText = result
+   
+    }else{
+        errorPeople.innerText = "Can't be zero"
+        inputPeopleContent.style.border = '1px solid red'
+        console.log(inputPeopleContent.style)
+    }
+    
      
   
 
@@ -26,24 +37,61 @@ form.addEventListener('click',function(e){
         const result = inputBill.value*0.10
         tipAmountResult.innerText = result
 
+        if(result!=NaN && result!=Infinity){
+            tipAmountResult.innerText = result
+       
+        }else{
+            errorPeople.innerText = "Can't be zero"
+            inputPeopleContent.style.border = '1px solid red'
+            console.log(errorPeople)
+        }
+        
+
     }else if(evento==btn15Perc){
 
         const result = inputBill.value*0.15
         tipAmountResult.innerText = result
+
+        if(result!=NaN && result!=Infinity){
+            tipAmountResult.innerText = result
+       
+        }else{
+            errorPeople.innerText = "Can't be zero"
+            inputPeopleContent.style.border = '1px solid red'
+            console.log(errorPeople)
+        }
+        
 
     }else if(evento==btn25Perc){
 
         const result = inputBill.value*0.25
         tipAmountResult.innerText = result
 
+        if(result!=NaN && result!=Infinity){
+            tipAmountResult.innerText = result
+       
+        }else{
+            errorPeople.innerText = "Can't be zero"
+            inputPeopleContent.style.border = '1px solid red'
+            console.log(errorPeople)
+        }
+        
+
     }else if(btn50Perc){
         
         const result = inputBill.value*0.50
         tipAmountResult.innerText = result
 
-    }else{
-        evento=" "
-        result = 0
+        if(result!=NaN && result!=Infinity){
+            tipAmountResult.innerText = result
+       
+        }else{
+            errorPeople.innerText = "Can't be zero"
+            inputPeopleContent.style.border = '1px solid red'
+            console.log(errorPeople)
+        }
+        
+
     }
 
     
