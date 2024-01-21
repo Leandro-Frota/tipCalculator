fa2fcca38b6ce4a898181f11383f09635baff1c2
Leandro-Frota <108706993+Leandro-Frota@users.noreply.github.com>
2024-01-21 20:51:12 -0300

    Adicionei calculo do valor total da conta  incluso o valor da gorjeta e fixei duas casas decimais, e refatorei arquivo script

--- script.js
+++ script.js
@@ -8,92 +8,85 @@ const btn25Perc = document.getElementById('btn25Perc');
 const btn50Perc = document.getElementById('btn50Perc');
 const tipAmountResult = document.getElementById('tipAmountResult')
 const inputPeople = document.getElementById('inputPeople')
-const inputPeopleContent= document.getElementById('inputPeopleContent')
+const inputPeopleContent = document.getElementById('inputPeopleContent')
+const tipAccountResult = document.getElementById('tipAccountResult')
 
 
-buttonSelectTips.addEventListener('click',function(e){
+buttonSelectTips.addEventListener('click', function (e) {
     const evento = e.target
     const people = inputPeople.value
+    const bill = inputBill.value
     errorPeople.innerText = ""
- 
-   
-    if(evento == btn5Perc){
- 
-    result = (inputBill.value*0.05)/people
 
-    if(result!=NaN && result!=Infinity){
-        tipAmountResult.innerText = result
-   
-    }else{
-        errorPeople.innerText = "Can't be zero"
-        inputPeopleContent.style.border = '1px solid red'
-        console.log(inputPeopleContent.style)
-    }
-    
-     
-  
+    if (evento == btn5Perc) {
 
-    }else if(evento==btn10Perc){
-        const result = inputBill.value*0.10
-        tipAmountResult.innerText = result
+        const result = (bill * 0.05) / people
+        const resultPorPerson = (bill / people) + result
 
-        if(result!=NaN && result!=Infinity){
-            tipAmountResult.innerText = result
-       
-        }else{
+        if (result != NaN && result != Infinity) {
+            tipAmountResult.innerText = result.toFixed(2)
+            tipAccountResult.innerText = resultPorPerson.toFixed(2)
+
+        } else {
             errorPeople.innerText = "Can't be zero"
             inputPeopleContent.style.border = '1px solid red'
-            console.log(errorPeople)
+
         }
-        
+    } else if (evento == btn10Perc) {
 
-    }else if(evento==btn15Perc){
+        const result = (bill * 0.10) / people
+        const resultPorPerson = (bill / people) + result
 
-        const result = inputBill.value*0.15
-        tipAmountResult.innerText = result
+        if (result != NaN && result != Infinity) {
+            tipAmountResult.innerText = result.toFixed(2)
+            tipAccountResult.innerText = resultPorPerson.toFixed(2)
 
-        if(result!=NaN && result!=Infinity){
-            tipAmountResult.innerText = result
-       
-        }else{
+        } else {
             errorPeople.innerText = "Can't be zero"
             inputPeopleContent.style.border = '1px solid red'
-            console.log(errorPeople)
         }
-        
+    } else if (evento == btn15Perc) {
 
-    }else if(evento==btn25Perc){
+        const result = (bill * 0.15) / people
+        const resultPorPerson = (bill / people) + result
 
-        const result = inputBill.value*0.25
-        tipAmountResult.innerText = result
+        if (result != NaN && result != Infinity) {
+            tipAmountResult.innerText = result.toFixed(2)
+            tipAccountResult.innerText = resultPorPerson.toFixed(2)
 
-        if(result!=NaN && result!=Infinity){
-            tipAmountResult.innerText = result
-       
-        }else{
+        } else {
             errorPeople.innerText = "Can't be zero"
             inputPeopleContent.style.border = '1px solid red'
-            console.log(errorPeople)
         }
-        
+    } else if (evento == btn25Perc) {
 
-    }else if(btn50Perc){
-        
-        const result = inputBill.value*0.50
-        tipAmountResult.innerText = result
+        const result = (bill * 0.25) / people
+        const resultPorPerson = (bill / people) + result
 
-        if(result!=NaN && result!=Infinity){
-            tipAmountResult.innerText = result
-       
-        }else{
+        if (result != NaN && result != Infinity) {
+            tipAmountResult.innerText = result.toFixed(2)
+            tipAccountResult.innerText = resultPorPerson.toFixed(2)
+        } else {
             errorPeople.innerText = "Can't be zero"
             inputPeopleContent.style.border = '1px solid red'
-            console.log(errorPeople)
-        }
-        
 
+        }
+
+    } else if (btn50Perc) {
+
+        const result = (bill * 0.50) / people
+        const resultPorPerson = (bill / people) + result
+
+        if (result != NaN && result != Infinity) {
+            tipAmountResult.innerText = result.toFixed(2)
+            tipAccountResult.innerText = resultPorPerson.toFixed(2)
+
+        } else {
+            errorPeople.innerText = "Can't be zero"
+            inputPeopleContent.style.border = '1px solid red'
+
+        }
     }
 
-    
 })
 
