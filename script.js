0530ed518e909ee1a2b63e6eb4ee73f38ae2bdef
Leandro-Frota <108706993+Leandro-Frota@users.noreply.github.com>
2024-01-22 22:31:19 -0300

    Adicionei feature ao tip custom com validação do campo people e bill , refatorei o codigo script colocando evento para cada botao

--- script.js
+++ script.js
@@ -10,93 +10,129 @@ const inputPercTip = document.getElementById('inputPercTip')
 const tipAmountResult = document.getElementById('tipAmountResult')
 const inputPeople = document.getElementById('inputPeople')
 const inputPeopleContent = document.getElementById('inputPeopleContent')
+const inputBillContent = document.getElementById('inputBillContent')
 const tipAccountResult = document.getElementById('tipAccountResult')
 const btnReset = document.getElementById('btnReset')
+const errorPeople = document.getElementById('errorPeople')
 
-console.log(btnReset)
 
-function recarregarPagina(){
+function recarregarPagina() {
     window.location.reload()
 }
 
-buttonSelectTips.addEventListener('click', function (e) {
-    const evento = e.target
+form.btn5Perc.addEventListener('click', function () {
+    inputPeopleContent.style.border = "none"
+    inputBillContent.style.border = "none"
     const people = inputPeople.value
     const bill = inputBill.value
-   
+    const result = (bill * 0.05) / people
+    const resultPorPerson = (bill / people) + result
     errorPeople.innerText = ""
 
+    if (people > 0 && bill > 0) {
+        tipAmountResult.innerText = result.toFixed(2)
+        tipAccountResult.innerText = resultPorPerson.toFixed(2)
+    } else {
+        errorPeople.innerText = "Can't be zero"
+        inputPeopleContent.style.border = '1px solid red'
+    }
+})
 
-    if (evento == btn5Perc) {
+form.btn10Perc.addEventListener('click', function () {
+    inputPeopleContent.style.border = "none"
+    inputBillContent.style.border = "none"
+    const people = inputPeople.value
+    const bill = inputBill.value
+    const result = (bill * 0.10) / people
+    const resultPorPerson = (bill / people) + result
+    errorPeople.innerText = ""
 
-        const result = (bill * 0.05) / people
-        const resultPorPerson = (bill / people) + result
-
-        if (result != NaN && result != Infinity) {
-            tipAmountResult.innerText = result.toFixed(2)
-            tipAccountResult.innerText = resultPorPerson.toFixed(2)
-
-        } else {
-            errorPeople.innerText = "Can't be zero"
-            inputPeopleContent.style.border = '1px solid red'
-
-        }
-    } else if (evento == btn10Perc) {
-
-        const result = (bill * 0.10) / people
-        const resultPorPerson = (bill / people) + result
-
-        if (result != NaN && result != Infinity) {
-            tipAmountResult.innerText = result.toFixed(2)
-            tipAccountResult.innerText = resultPorPerson.toFixed(2)
-
-        } else {
-            errorPeople.innerText = "Can't be zero"
-            inputPeopleContent.style.border = '1px solid red'
-        }
-    } else if (evento == btn15Perc) {
-
-        const result = (bill * 0.15) / people
-        const resultPorPerson = (bill / people) + result
-
-        if (result != NaN && result != Infinity) {
-            tipAmountResult.innerText = result.toFixed(2)
-            tipAccountResult.innerText = resultPorPerson.toFixed(2)
-
-        } else {
-            errorPeople.innerText = "Can't be zero"
-            inputPeopleContent.style.border = '1px solid red'
-        }
-    } else if (evento == btn25Perc) {
-
-        const result = (bill * 0.25) / people
-        const resultPorPerson = (bill / people) + result
-
-        if (result != NaN && result != Infinity) {
-            tipAmountResult.innerText = result.toFixed(2)
-            tipAccountResult.innerText = resultPorPerson.toFixed(2)
-        } else {
-            errorPeople.innerText = "Can't be zero"
-            inputPeopleContent.style.border = '1px solid red'
-
-        }
-
-    } else if (btn50Perc == btn50Perc ) {
-
-        const result = (bill * 0.50) / people
-        const resultPorPerson = (bill / people) + result
-
-        if (result != NaN && result != Infinity) {
-            tipAmountResult.innerText = result.toFixed(2)
-            tipAccountResult.innerText = resultPorPerson.toFixed(2)
-
-        } else {
-            errorPeople.innerText = "Can't be zero"
-            inputPeopleContent.style.border = '1px solid red'
-
-        }
+    if (people > 0 && bill > 0) {
+        tipAmountResult.innerText = result.toFixed(2)
+        tipAccountResult.innerText = resultPorPerson.toFixed(2)
+    } else {
+        errorPeople.innerText = "Can't be zero"
+        inputPeopleContent.style.border = '1px solid red'
     }
 
+})
+
+form.btn15Perc.addEventListener('click', function () {
+    inputPeopleContent.style.border = "none"
+    inputBillContent.style.border = "none"
+    const people = inputPeople.value
+    const bill = inputBill.value
+    const result = (bill * 0.15) / people
+    const resultPorPerson = (bill / people) + result
+    errorPeople.innerText = ""
+
+    if (people > 0 && bill > 0) {
+        tipAmountResult.innerText = result.toFixed(2)
+        tipAccountResult.innerText = resultPorPerson.toFixed(2)
+    } else {
+        errorPeople.innerText = "Can't be zero"
+        inputPeopleContent.style.border = '1px solid red'
+    }
+})
+
+form.btn25Perc.addEventListener('click', function () {
+    inputPeopleContent.style.border = "none"
+    inputBillContent.style.border = "none"
+    const people = inputPeople.value
+    const bill = inputBill.value
+    const result = (bill * 0.25) / people
+    const resultPorPerson = (bill / people) + result
+    errorPeople.innerText = ""
+
+    if (people > 0 && bill > 0) {
+        tipAmountResult.innerText = result.toFixed(2)
+        tipAccountResult.innerText = resultPorPerson.toFixed(2)
+    } else {
+        errorPeople.innerText = "Can't be zero"
+        inputPeopleContent.style.border = '1px solid red'
+    }
+})
+
+form.btn50Perc.addEventListener('click', function () {
+    inputPeopleContent.style.border = "none"
+    inputBillContent.style.border = "none"
+    const people = inputPeople.value
+    const bill = inputBill.value
+    const result = (bill * 0.50) / people
+    const resultPorPerson = (bill / people) + result
+    errorPeople.innerText = ""
+
+    if (people > 0 && bill > 0) {
+        tipAmountResult.innerText = result.toFixed(2)
+        tipAccountResult.innerText = resultPorPerson.toFixed(2)
+    } else {
+        errorPeople.innerText = "Can't be zero"
+        inputPeopleContent.style.border = '1px solid red'
+    }
+})
+
+form.inputPercTip.addEventListener('change', function () {
+
+    const customTip = inputPercTip.value
+    const people = inputPeople.value
+    const bill = inputBill.value
+    const result = (bill * (customTip / 100)) / people
+    const resultPorPerson = (bill / people) + result
+    errorPeople.innerText = ""
+
+    console.log(result)
+
+    if (people > 0 && bill > 0) {
+        tipAmountResult.innerText = result.toFixed(2)
+        tipAccountResult.innerText = resultPorPerson.toFixed(2)
+        inputPeopleContent.style.border = "none"
+        inputBillContent.style.border = "none"
+    } else {
+        errorPeople.innerText = "Can't be zero"
+        inputPeopleContent.style.border = '1px solid red'
+        inputBillContent.style.border = '1px solid red'
+        inputPercTip.value = ""
+    }
 })
 
 btnReset.addEventListener('click', recarregarPagina)
